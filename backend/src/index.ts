47a2ea3e4903a022aa7476bef261d82f2b8034ed
Kailer Laino <kailer@meta.com>
2025-06-24 12:49:30 -0700

    connect auth route to index file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,30 @@
 import dotenv from "dotenv";
-dotenv.config();
-
 import express, { Request, Response, Application } from "express";
+const session = require("express-session");
 
-const app: Application = express();
-
+dotenv.config();
 const PORT: number = Number(process.env.PORT) || 5000;
 
+// Import routes
+const authRouter = require("./routes/auth");
+
+const app: Application = express();
 app.use(express.json());
+
+let sessionConfig = {
+  name: "sessionId",
+  secret: process.env.SESSION_SECRET || "defaultSecret",
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24, // 1 day
+    secure: process.env.RENDER ? true: false,
+    httpOnly: false,
+  },
+  resave: false,
+  saveUninitialized: false,
+}
+
+app.use(session(sessionConfig));
+app.use(authRouter);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
